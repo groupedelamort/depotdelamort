1e2b5321e30681579eca8a89c31a015c73024fd8
AJC-Formation <AJC-Formation@ajc-PC>
2015-04-02 16:30:55 +0200

    Metier Client Benjamin

--- ProjetDeLaMort/src/com/groupe/gestion/compte/metier/ClientReunionId.java
+++ ProjetDeLaMort/src/com/groupe/gestion/compte/metier/ClientReunionId.java
@@ -2,6 +2,72 @@ package com.groupe.gestion.compte.metier;
 
 import java.io.Serializable;
 
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+@Embeddable
 public class ClientReunionId implements Serializable{
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Client client;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Reunion reunion;
+	
+
+	public ClientReunionId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Reunion getReunion() {
+		return reunion;
+	}
+
+	public void setReunion(Reunion reunion) {
+		this.reunion = reunion;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((client == null) ? 0 : client.hashCode());
+		result = prime * result + ((reunion == null) ? 0 : reunion.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClientReunionId other = (ClientReunionId) obj;
+		if (client == null) {
+			if (other.client != null)
+				return false;
+		} else if (!client.equals(other.client))
+			return false;
+		if (reunion == null) {
+			if (other.reunion != null)
+				return false;
+		} else if (!reunion.equals(other.reunion))
+			return false;
+		return true;
+	}
+	
+	
 
 }
--- ProjetDeLaMort/src/com/groupe/gestion/compte/metier/Login.java
+++ ProjetDeLaMort/src/com/groupe/gestion/compte/metier/Login.java
@@ -3,17 +3,34 @@ package com.groupe.gestion.compte.metier;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import javax.persistence.Version;
 
 @Entity
+@Table(name="Logins")
 public class Login implements Serializable {
-	
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long idLogin;
 	private String login;
 	private String mdp;
 	
+	@OneToOne(mappedBy="login")
+	private Client client;
+	
 	@Version
 	private int version;
+	
+	
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	
 	public long getIdLogin() {
 		return idLogin;
@@ -40,6 +57,61 @@ public class Login implements Serializable {
 		this.version = version;
 	}
 	
-	
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((client == null) ? 0 : client.hashCode());
+		result = prime * result + (int) (idLogin ^ (idLogin >>> 32));
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((mdp == null) ? 0 : mdp.hashCode());
+		result = prime * result + version;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Login other = (Login) obj;
+		if (client == null) {
+			if (other.client != null)
+				return false;
+		} else if (!client.equals(other.client))
+			return false;
+		if (idLogin != other.idLogin)
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (mdp == null) {
+			if (other.mdp != null)
+				return false;
+		} else if (!mdp.equals(other.mdp))
+			return false;
+		if (version != other.version)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Login [idLogin=" + idLogin + ", login=" + login + ", mdp="
+				+ mdp + ", client=" + client + "]";
+	}
 	
 }
