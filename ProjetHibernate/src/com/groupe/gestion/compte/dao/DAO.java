50b8f705b0f111f23de5a4f0350e503a26d13bad
AJC Formation <AJC Formation@ajc-PC>
2015-04-02 15:46:28 +0200

    interface DAO

---
+++ ProjetHibernate/src/com/groupe/gestion/compte/dao/DAO.java
@@ -0,0 +1,13 @@
+package com.groupe.gestion.compte.dao;
+
+import java.util.List;
+
+public interface DAO<T> {
+
+	
+	public void create (T arg0);
+	public List<T> readAll ();
+	public T readById (Long id);
+	public void update (T arg0);
+	public void delete (T arg0);
+}
